374bc9bc34dba28305a1514ac09d99885b75c067
rajeshwaran1142003 <159461167+rajeshwaran1142003@users.noreply.github.com>
2025-08-03 10:25:59 +0530

    Remove Store Locator section

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import Recipes from './components/Recipes';
 import UsageIdeas from './components/UsageIdeas';
 import Blog from './components/Blog';
 import Gallery from './components/Gallery';
-import StoreLocator from './components/StoreLocator';
 import ShippingReturns from './components/ShippingReturns';
 import FAQ from './components/FAQ';
 import Contact from './components/Contact';
@@ -24,7 +23,6 @@ function App() {
       <UsageIdeas />
       <Blog />
       <Gallery />
-      <StoreLocator />
       <ShippingReturns />
       <FAQ />
       <Contact />
